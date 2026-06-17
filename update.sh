2adc046f2f3d590994fe5ebadc435bb0d72229e6
Charlie Laughton <CharlieLaughton@users.noreply.github.com>
2026-06-17 11:06:26 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-git clone https://github.com/CompBioAsia/Amber-md-prep
 pip install numpy==2.2.6
-pip install mdtraj==1.11.1.post1
+pip install mdtraj==1.11.1
 pip install git+https://github.com/CharlieLaughton/Alphafix.git
 pip install git+https://github.com/CompBioAsia/CBAtools.git
